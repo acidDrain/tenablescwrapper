89fe6fe84dfee3ed981e30b73430f3456232fe1b
wherepasswordequals james.grow@fishtech.group <james.grow@fishtech.group>
2018-12-13 16:25:39 -0600

    Changed script to read authentication and host information from a config file instead of environment

--- example.py
+++ example.py
@@ -6,19 +6,23 @@ from tenablescwrapper.analysis import analysis
 
 disable_warnings(exceptions.InsecureRequestWarning)
 
+# Get authentication and hostname information for Tenable from config.json file
+with open('./config.json', 'r') as config_f:
+    config = json.load(config_f)
+
+credentials = {'username': config['username'], 'password': config['password']}
+
+security_center_hostname = config['sc_host']
+
+# Read the Vulnerability Detail query from file (which will be submitted to /analysis API endpoint)
 with open('./queries/vulndetail_query.json', 'r') as vuln_query_f:
     vulndetail_query = json.load(vuln_query_f)
 
-sc_username = getenv('SC_USERNAME')
-sc_password = getenv('SC_PASSWORD')
-
-credentials = {'username': sc_username, 'password': sc_password}
-
-security_center_hostname = 'tenable.nodomain.lan'
-
+# Authenticate to Tenable Security Center API
 security_center_auth_data = authenticate(
     security_center_hostname, credentials)
 
+# Query the Tenable Security Center /analysis API endpoint for Vulnerability Details
 vulndetail_results = analysis(
     security_center_auth_data['headers'],
     security_center_auth_data['cookies'],
@@ -26,4 +30,5 @@ vulndetail_results = analysis(
     json.dumps(vulndetail_query)
 )
 
+# Print results
 print(json.dumps(vulndetail_results))
